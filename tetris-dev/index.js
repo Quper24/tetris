57f233f2c588301a2bd585cc3f482e37737acb46
ЬфлMaksim Leskin <maksim.leskin@methed.ru>
2022-04-19 02:39:53 +0300

    module

--- tetris-dev/index.js
+++ tetris-dev/index.js
@@ -1,150 +1,9 @@
+import { Game } from "./module/game.js";
 const SIZE_BLOCK = 25;
 
 // механика
 
-class Game {
-  area = [
-    ['o','o','o','o','o','o','o','o','o','o',],
-    ['o','o','o','o','o','o','o','o','o','o',],
-    ['o','o','o','o','o','o','o','o','o','o',],
-    ['o','o','o','o','o','o','o','o','o','o',],
-    ['o','o','o','o','o','o','o','o','o','o',],
-    ['o','o','o','o','o','o','o','o','o','o',],
-    ['o','o','o','o','o','o','o','o','o','o',],
-    ['o','o','o','o','o','o','o','o','o','o',],
-    ['o','o','o','o','o','o','o','o','o','o',],
-    ['o','o','o','o','o','o','o','o','o','o',],
-    ['o','o','o','o','o','o','o','o','o','o',],
-    ['o','o','o','o','o','o','o','o','o','o',],
-    ['o','o','o','o','o','o','o','o','o','o',],
-    ['o','o','o','o','o','o','o','o','o','o',],
-    ['o','o','o','o','o','o','o','o','o','o',],
-    ['o','o','o','o','o','o','o','o','o','o',],
-    ['o','o','o','o','o','o','o','o','o','x',],
-    ['o','o','o','o','o','o','o','o','o','x',],
-    ['o','o','o','o','o','x','o','o','o','x',],
-    ['o','o','o','o','x','x','x','o','o','x',],
-  ];
-  
-  activeTetromino = {
-    x: 3,
-    y: 0,
-    block: [
-      ['o', 'x', 'o'],
-      ['o', 'x', 'o'],
-      ['x', 'x', 'o'],
-    ],
-    rotationIndex: 0,
-    rotation: [
-      [
-        ['o', 'x', 'o'],
-        ['o', 'x', 'o'],
-        ['x', 'x', 'o'],
-      ],
-      [
-        ['x', 'o', 'o'],
-        ['x', 'x', 'x'],
-        ['o', 'o', 'o'],
-      ],
-      [
-        ['o', 'x', 'x'],
-        ['o', 'x', 'o'],
-        ['o', 'x', 'o'],
-      ],
-      [
-        ['o', 'o', 'o'],
-        ['x', 'x', 'x'],
-        ['o', 'o', 'x'],
-      ],
-    ]
-  };
 
-
-
-  moveLeft() {
-    if (this.checkOutPosition(this.activeTetromino.x - 1, this.activeTetromino.y)) {
-      this.activeTetromino.x -= 1;
-    }
-  };
-
-  moveRight() {
-    if (this.checkOutPosition(this.activeTetromino.x + 1, this.activeTetromino.y)) {
-      this.activeTetromino.x += 1;
-    }
-  };
-
-  moveDown() {
-    if (this.checkOutPosition(this.activeTetromino.x, this.activeTetromino.y + 1)) {
-      this.activeTetromino.y += 1;
-    } else {
-      this.stopMove();
-    }
-  };
-
-  rotateTetromino() {
-    this.activeTetromino.rotationIndex =
-      this.activeTetromino.rotationIndex < 3 ?
-        this.activeTetromino.rotationIndex + 1 : 0;
-
-    this.activeTetromino.block =
-      this.activeTetromino.rotation[this.activeTetromino.rotationIndex];
-      
-    if (!this.checkOutPosition(this.activeTetromino.x, this.activeTetromino.y)) {
-      this.activeTetromino.rotationIndex =
-        this.activeTetromino.rotationIndex > 0 ?
-          this.activeTetromino.rotationIndex - 1 : 3;
-
-      this.activeTetromino.block =
-        this.activeTetromino.rotation[this.activeTetromino.rotationIndex];
-    }
-  };
-
-  get viewArea() {
-    const area = JSON.parse(JSON.stringify(this.area));
-    const {x, y, block: tetromino} = this.activeTetromino;
-
-    for(let i = 0; i < tetromino.length; i++) {
-      const row = tetromino[i];
-      for (let j = 0; j < row.length; j++) {
-        if (row[j] === 'x') {
-          area[y + i][x + j] = tetromino[i][j];
-        }
-      }
-    }
-
-    return area;
-  };
-
-  checkOutPosition(x, y) {
-    const tetromino = this.activeTetromino.block;
-    for(let i = 0; i < tetromino.length; i++) {
-      for (let j = 0; j < tetromino[i].length; j++) {
-        if (tetromino[i][j] === 'o') continue;
-
-        if (!this.area[y + i] ||
-          !this.area[y + i][x + j] ||
-          this.area[y + i][x + j] === 'x') {
-          return false;
-        }
-        
-      }
-    }
-    return true
-  };
-
-  stopMove() {
-    const {x, y, block: tetromino} = this.activeTetromino;
-
-    for(let i = 0; i < tetromino.length; i++) {
-      const row = tetromino[i];
-      for (let j = 0; j < row.length; j++) {
-        if (row[j] === 'x') {
-          this.area[y + i][x + j] = tetromino[i][j];
-        }
-      }
-    }
-  }
-}
 
 const game = new Game();
 
@@ -178,29 +37,29 @@ const showArea = area => {
     }
   }
 
-  
+
 };
 
 window.addEventListener('keydown', event => {
   const key = event.code;
-  
+
   switch (key) {
     case 'ArrowLeft':
       game.moveLeft();
       showArea(game.viewArea);
-    break;
+      break;
     case 'ArrowRight':
       game.moveRight();
       showArea(game.viewArea);
-    break;
+      break;
     case 'ArrowDown':
       game.moveDown();
       showArea(game.viewArea);
-    break;
+      break;
     case 'ArrowUp':
       game.rotateTetromino();
       showArea(game.viewArea);
-    break;
+      break;
   }
 });
 
